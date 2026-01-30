47e522bc4c135221c672cdc9534865195753a081
Nevan <nevankaiane@gmail.com>
2026-01-30 19:10:36 +0700

    Complete calculator with basic and advance operations

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// test
-console.log("Hello")
+// display
 
 const display = document.getElementById('display')
+
+// basic function
 
 function appendToDisplay(value) {
   if (display.value === '0' && value !== '.') {
@@ -28,5 +29,35 @@ function deleteLast() {
     display.value = display.value.slice(0, -1);
   } else {
     display.value = '0';
+  }
+}
+
+// advanced function
+
+function square() {
+  const numSq = parseFloat(display.value);
+  if (!isNaN(num)) {
+    const resultSq = numSq * numSq;
+    display.value = resultSq.toString();
+  }
+}
+
+function inverse() {
+  const numIv = parseFloat(display.value);
+  if (!isNaN(numIv) && numIv !== 0) {
+    const resultIv = 1 / numIv;
+    display.value = resultIv.toString();
+  } else {
+    display.value = "Error";
+  }
+}
+
+function squareRoot() {
+  const numSr = parseFloat(display.value);
+  if (!isNaN(numSr)) {
+    const resultSr = Math.sqrt(numSr);
+    display.value = resultSr.toString();
+  } else {
+    display.value = "Error";
   }
 }
